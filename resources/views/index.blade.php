3b6b501cf5d89f169e8fd0ad7c64a61f2c8176cd
Salat-Tamas <tamas.salat2004@gmail.com>
2024-10-26 17:30:37 +0300

    started implementing some design ideas

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.master')
 
 @section('content')
-    <h2 class="text-2xl text-red-500">
-        Some databases go here!
-    </h2>
+   
 @endsection
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -5,19 +5,23 @@
     <title>Databases Partial Project</title>
     @vite('resources/css/app.css')
 </head>
-<body class="px-5 py-2">
-    <nav>
-        <div>
-            <h1>Database Manager</h1>
+<body>
+    <nav class="border-b-2 bg-black/5 border-black/25 w-full p-2">
+        <div class="flex space-x-5 items-center">
+            <div>
+                <img src="{{ asset('images/logosapi.png') }}" alt="Logo" class="h-16"/>
+            </div>
+            <h1 class="text-4xl font-serif font-bold text-green-800 outline-4 outline-gray-600">EduGraph</h1>
         </div>
     </nav>
 
-    <main class="h-full">
+    <main class="h-full px-5 pt-5">
         @yield('content')
     </main>
 
-    <footer class="border-t border-black/20 p-y-2">
+    <footer class="border-t border-black/20 py-2 mx-5">
         <p>&copy; 2021 Database Manager</p>
     </footer>
+    @vite('resources/js/app.js')
 </body>
 </html>
